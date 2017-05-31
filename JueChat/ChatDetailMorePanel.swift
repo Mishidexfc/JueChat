180ac6d9f6761934f3ac5138617e75f1757690d0
Mishidexfc <juewang@JuedeMacBook-Air.local>
2017-05-31 14:11:26 +0800

    add more panel

---
+++ JueChat/ChatDetailMorePanel.swift
@@ -0,0 +1,18 @@
+//
+//  ChatDetailMorePanel.swift
+//  JueChat
+//
+//  Created by Jue Wang on 2017/5/31.
+//  Copyright © 2017年 Syracuse University. All rights reserved.
+//
+
+import UIKit
+class ChatDetailMorePanel:UIView{
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.drawLayout()
+    }
+    private func drawLayout(){
+    
+    }
+}
